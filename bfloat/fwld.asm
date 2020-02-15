01da52c6bc7f5d38ddfeaa12a50f78a628374019
DW0RKiN <ogion@centrum.cz>
2020-02-15 16:33:47 +0000

    Add files via upload

--- bfloat/fwld.asm
+++ bfloat/fwld.asm
@@ -1,8 +1,5 @@
 if not defined @FWLD
 
-include "fbld.asm"
-include "faddp.asm"
-
 ; Load Word. Convert unsigned 16-bit integer into floating-point number
 ;  In: HL = Word to convert
 ; Out: HL = floating point representation
@@ -13,41 +10,40 @@ if not defined FWLD
                     FWLD                ; *
 ; *****************************************
 endif
-        PUSH    DE                  ;  1:11        
         LD      A, H                ;  1:4
         OR      A                   ;  1:4
-        JR      z, FWLD_ONLY_LO     ;  2:12/7
-
-        CALL    FBLD                ;  3:17     DE = 1.0 * A
-    if 1
-        LD      A, EXP_PLUS_ONE << 3;  2:7
-        ADD     A, D                ;  1:4
+        JR      z, FWLD_BYTE        ;  2:12/7
+        LD      A, BIAS+16          ;  2:7          HL = xxxx xxxx xxxx xxxx
+FWLD_NORM:
+        ADD     HL, HL              ;  1:11
+        DEC     A                   ;  1:4
+        JP      nc, FWLD_NORM       ;  3:10       
+        
+        SRL     H                   ;  2:8
+        CCF                         ;  1:4
+        RR      L                   ;  2:8          rounding
+        LD      L, H                ;  1:4
         LD      H, A                ;  1:4
-        LD      A, L                ;  1:4
-        LD      L, E                ;  1:4        
-                                    ;  6:23
-    else
-        LD      A, L                ;  1:4        
-        LD      HL, EXP_PLUS_ONE<<11;  3:10
-        ADD     HL, DE              ;  1:11
-                                    ;  5:25
-    endif
-        OR      A                   ;  1:4
-        JR      z, FWLD_ONLY_HI     ;  2:12/7
-        CALL    FBLD                ;  3:17     DE = 1.0 * A
-        PUSH    BC                  ;  1:11
-        CALL    FADDP               ;  3:17     HL = HL + DE
-        POP     BC                  ;  1:10
-FWLD_ONLY_HI:
-        POP     DE                  ;  1:10
+        RET     m                   ;  1:11/5
+        RET     z                   ;  1:11/5
+        INC     L                   ;  1:4          rounding up
+        RET     p                   ;  1:11/5        
+        INC     H                   ;  1:4          exp++
+        LD      L, $00              ;  2:7
         RET                         ;  1:10
-                                    ; 27/26:152/154 + 2*FBLD + FADDP
+        
+FWLD_BYTE:                          ;               HL = 0000 0000 xxxx xxxx
+        OR      L                   ;  1:4
+        RET     z                   ;  1:5/11
+                
+        LD      H, BIAS+8           ;  2:7
+FWLD_BYTE_NORM:
+        DEC     H                   ;  1:4
+        ADD     A, A                ;  1:4
+        JR      nc, FWLD_BYTE_NORM  ;  2:12/7
+        
+        RRCA                        ;  1:4
+        LD      L, A                ;  1:4
+        RET                         ;  1:10
 
-FWLD_ONLY_LO:
-        LD      A, L                ;  1:4
-        CALL    FBLD                ;  3:17     DE = 1.0 * A
-        EX      DE, HL              ;  1:4
-        POP     DE                  ;  1:10
-        RET                         ;  1:10
-                                    ; 33:
 endif
