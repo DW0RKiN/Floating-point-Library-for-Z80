33d301dee817ed73c61d4ac2e6c62f1fcd94400f
DW0RKiN <ogion@centrum.cz>
2020-02-17 12:33:53 +0000

    Add files via upload

---
+++ binary16/fabs.asm
@@ -0,0 +1,16 @@
+if not defined FABS
+
+; Warning: must be included before first use!
+; Returns the absolute value of a number.
+;   Input: reg_hi, reg_lo
+;  Output: reg = abs(reg)
+;     Use: FABS H,L
+FABS MACRO reg_hi, reg_lo
+    if SIGN_BIT > 7
+        RES     8-SIGN_BIT, reg_hi  ;  2:8      reg = abs(reg)
+    else
+        RES     SIGN_BIT, reg_lo    ;  2:8      reg = abs(reg)
+    endif
+ENDM
+
+endif
--- binary16/fdiv2.asm
+++ binary16/fdiv2.asm
@@ -3,9 +3,9 @@ if not defined FDIV2
 ; Warning: must be included before first use!
 ;   Input: reg_hi, reg_lo
 ;  Output: number /= 2
-FMUL2 MACRO reg_hi, reg_lo
+FDIV2 MACRO reg_hi, reg_lo
     if EXP_PLUS_ONE > 1
-        LD      A, reg_hi           ;  1:4
+        LD      A, reg_hi           ;  1:4          4*DEC reg_hi
         SUB     A, EXP_PLUS_ONE     ;  2:7
         .WARNING EXP muze podtect!
         LD      reg_hi, A           ;  1:4          number /= 2
